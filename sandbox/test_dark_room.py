30fe50b52b330dce1cc5df3a50402038f8da5b54
cinemere <albina.k.p16kaa4@gmail.com>
2024-07-04 22:10:46 +0300

    try dark room env

---
+++ sandbox/test_dark_room.py
@@ -0,0 +1,15 @@
+# %%
+from toymeta.dark_room import DarkRoom
+import matplotlib.pyplot as plt
+
+env = DarkRoom(terminate_on_goal=True)
+# %%
+# %%
+obs, info = env.reset()
+done = False
+while not done:
+    obs, reward, done, _, info = env.step(env.action_space.sample())
+    print(obs, reward, info)
+    plt.imshow(env.render())
+    plt.show()
+# %%
