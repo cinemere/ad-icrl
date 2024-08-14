df961bf925f5c83c68bd657d83187f00a4e666dc
cinemere <albina.k.p16kaa4@gmail.com>
2024-08-14 21:56:10 +0300

    add eval script

---
+++ scripts/eval.sh
@@ -0,0 +1,5 @@
+PYTHONPATH=. \
+python3 src/check_in_context/eval_one_model.py \
+--model-dir /home/cinemere/work/repo/ad-icrl/saved_data/saved_models/colab-model2 \
+--n-repeats 5 \
+--eval-episodes 100 
